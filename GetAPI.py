5c852e1d6d71160f7336a0f39acdf6d63d7fce6b
NEM1S1S <vupetersendan@gmail.com>
2021-02-05 08:07:49 +0100

    Kommentering af koden

--- GetAPI.py
+++ GetAPI.py
@@ -5,13 +5,28 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='652c67837d0f4ab3861ba5ba6c2e55c0')
 
 #Søgefeltet for artiklerne
-keyword = input('Indtast et søge ord: ')
+keyword = input('Indtast et søgeord: ')
 
 #Her hentes alt der indeholder søgeordet, er på engelsk og 20 sider langt.
 data = newsapi.get_everything(q= keyword ,language='en', page_size=20)
 
+#Printer statussen for koden, om det fungerer
+print('Status: ' + data['status'])
+
+#Hvor gange ordet er fundet.
+print('Total Results ')
+print(data['totalResults'])
+
+type(data['articles'])
+
+print(data['articles'][0])
+
 #Udprintning af dataen
 print(data)
+
+
+
+#requests commands
 
 #r = requests.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=652c67837d0f4ab3861ba5ba6c2e55c0')
 
