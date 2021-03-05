e9d1ef7d7632486ea257a5b29d8caddace6b2573
NEM1S1S <vupetersendan@gmail.com>
2021-03-05 18:41:52 +0100

    Fjernet unødvendig import af requests og videre arbejde af Test.py

--- Test.py
+++ Test.py
@@ -13,8 +13,8 @@ def getData(keywords):
     articles = data['articles']
 
     #Her optilles søgningens forskellige titler for de fundne artikler.
-    for x, y in enumerate(articles):
-        print(f'{x+1}    {y["title"]}')
+    for x, y, z in enumerate(articles):
+        print(f'{x+1}    {y["title"]}    {z["author"]}')
 
 
     return data
