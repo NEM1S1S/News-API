c7cbc014786ed13d325eec645d12c3dc42aa9a77
NEM1S1S <vupetersendan@gmail.com>
2021-03-05 21:29:42 +0100

    Another error

--- GetAPI.py
+++ GetAPI.py
@@ -4,7 +4,7 @@ def getData(keywords):
     #Her hentes dataen fra
     newsapi = NewsApiClient(api_key='652c67837d0f4ab3861ba5ba6c2e55c0')
 
-    #Her hentes alt der indeholder søgeordet, er på engelsk og 20 sider langt.
+    #Her hentes alt der indeholder søgeordet, er på engelsk og hvor mange artikler skal fremvises på en gang.
     data = newsapi.get_everything(q= keywords ,language='en', page_size=20)
 
     return data
